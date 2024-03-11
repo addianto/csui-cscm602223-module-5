badf544014e421da73d7be1d061ffdd5e5674be4
Daya Adianto <dayaadianto@cs.ui.ac.id>
2024-03-11 16:21:03 +0700

    chore: Use Spring Boot v3.2.3

--- src/test/java/com/advpro/profiling/tutorial/service/StudentServiceTest.java
+++ src/test/java/com/advpro/profiling/tutorial/service/StudentServiceTest.java
@@ -13,8 +13,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class StudentServiceTest {
